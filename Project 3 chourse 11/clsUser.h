4f3e055438740eeea3a8114a20765638db34c340
othman <hjmhmdthmanmrad@gmail.com>
2026-03-17 06:29:39 +0200

    Add clsUser from Lesson 18

---
+++ Project 3 chourse 11/clsUser.h
@@ -0,0 +1,294 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include "clsScreen.h"
+#include "clsPerson.h"
+#include "clsString.h"
+#include "clsBankClient.h"
+
+using namespace std;
+class clsUser : public clsPerson
+{
+
+private:
+	enum enMode { enEmptyMode = 0, enUpdateMode = 1, enAddNewMode = 2 };
+	enMode _Mode;
+	string _UserName;
+	string _Password;
+	string _Permissions;
+	bool _MarkedForDelete = false;	
+	
+	static clsUser _ConvertUserLineToUserObject(string UserLine, string Spreator ="#//#")
+	{
+		vector <string> vUserData;
+		vUserData = clsString::Split(UserLine, Spreator);
+		return clsUser(enMode::enEmptyMode, vUserData[1], vUserData[2], vUserData[3], vUserData[4], vUserData[5], vUserData[6], vUserData[7]);
+		
+	}
+	static string _ConvertUserObjectToUserLine(clsUser User , string Spreator = "#//#")
+	{
+		string UserLine = "";
+		UserLine += User.UserName + "#//#";
+		UserLine += User.FirstName + "#//#";
+		UserLine += User.LastName + "#//#";
+		UserLine += User.Email + "#//#";
+		UserLine += User.Phone + "#//#";
+		UserLine += User.Password + "#//#";
+		UserLine += User.Permission;
+		return UserLine;
+	}
+	static vector <clsUser> _LoadUsersDataFromFile()
+	{
+		vector <clsUser> vUsers;
+		
+		fstream MyFile;
+		MyFile.open("Users.txt", ios::in);
+		if (MyFile.is_open())
+		{
+			string Line;
+			while (getline(MyFile, Line))
+			{
+			 clsUser User = _ConvertUserLineToUserObject(Line);
+				vUsers.push_back(User);
+			}
+			MyFile.close();
+		}
+		return vUsers;	
+		
+	}
+	static void _SaveUsersDataToFile(vector <clsUser> vUsers)
+	{
+		fstream MyFile;
+		MyFile.open("Users.txt", ios::out);
+		if (MyFile.is_open())
+		{
+			for (clsUser User : vUsers)
+			{
+				if (User._MarkedForDelete==false)
+				{
+
+					string UserLine = _ConvertUserObjectToUserLine(User);
+					MyFile << UserLine << endl;
+
+				}
+			}
+			MyFile.close();
+		}
+
+	}
+	static clsUser _GetEmptyUserObject()
+	{
+		return clsUser(enEmptyMode, "", "", "", "", "", "", "");
+	}
+	
+	void _Update()
+	{
+		vector <clsUser> vUsers;
+		vUsers = _LoadUsersDataFromFile();
+		for (clsUser& User : vUsers)
+		{
+			if (User.UserName == UserName)
+			{
+				User = *this;
+				break;
+			}
+		}
+		_SaveUsersDataToFile(vUsers);
+	}
+	
+	void _AddDataLineToFile(string UserLine)
+	{
+		fstream MyFile;
+		MyFile.open("Users.txt", ios::out | ios::app);
+		if (MyFile.is_open())
+		{
+			MyFile << UserLine << endl;
+			MyFile.close();
+		}
+	}
+	void _AddNewUser()
+	{
+		_AddDataLineToFile(_ConvertUserObjectToUserLine(*this));
+	}
+
+public:
+	
+	clsUser(enMode Mode,string FirstName,string LastName,string Emaile,
+		string Phone,string UserName ,string Password,string Permissions):
+		clsPerson(FirstName, LastName, Emaile, Phone)
+	{
+		_Mode = Mode;
+		_UserName = UserName;
+		_Password = Password;
+		_Permissions = Permissions;
+	}
+
+	bool IsEmpty()
+	{
+		return (_Mode == enEmptyMode);
+	}
+
+
+	 void SetUserName(string UserName) 
+	 {
+		 _UserName = UserName;
+
+	 }
+	 string GetUserName() 
+	 {
+		 return _UserName;
+	 }
+	 _declspec(property(get = GetUserName, put = SetUserName)) string UserName;
+
+
+	 void SetPassword(string Password)
+	 { 
+		 _Password = Password;
+	 }
+	 string GetPassword()
+	 {
+		 return _Password;
+	 }
+	_declspec(property(get = GetPassword, put = SetPassword)) string Password;
+
+
+	 void SetPermissions(string Permissions)
+	{
+		_Permissions = Permissions;
+	}
+	 string GetPermissions() 
+	 { 
+		 return _Permissions;
+	 }
+	_declspec(property(get = GetPermissions, put = SetPermissions)) string Permission;
+
+	static clsUser Find(string UserName)
+	{
+
+
+		fstream MyFile;
+		MyFile.open("Users.txt", ios::in);//read Mode
+
+		if (MyFile.is_open())
+		{
+			string Line;
+			while (getline(MyFile, Line))
+			{
+				clsUser User = _ConvertUserLineToUserObject(Line);
+				if (User._UserName ==UserName)
+				{
+					MyFile.close();
+					return User;
+				}
+
+			}
+
+			MyFile.close();
+
+		}
+
+		return _GetEmptyUserObject();
+	}
+
+	static clsUser Find(string UserName ,string Password)
+	{
+
+
+		fstream MyFile;
+		MyFile.open("Users.txt", ios::in);//read Mode
+
+		if (MyFile.is_open())
+		{
+			string Line;
+			while (getline(MyFile, Line))
+			{
+				clsUser User = _ConvertUserLineToUserObject(Line);
+				if (User._UserName == UserName && User.Password==Password)
+				{
+					MyFile.close();
+					return User;
+				}
+
+			}
+
+			MyFile.close();
+
+		}
+
+		return _GetEmptyUserObject();
+	}
+
+	enum enSaveResults { svFaildEmptyObject = 0, svSucceeded = 1, svFaildUserNameExsist = 2 };
+	
+	enSaveResults Save()
+	{
+		switch (_Mode)
+		{
+		case enEmptyMode:
+		{
+			return enSaveResults::svFaildEmptyObject;
+		}
+		case enUpdateMode:
+		{
+			_Update();
+			return enSaveResults::svSucceeded;
+			break;
+		}
+		case enAddNewMode:
+		{
+			if (clsUser::IsUserExist(_UserName))
+			{
+				return enSaveResults::svFaildUserNameExsist;
+			}
+			else
+			{
+				_AddNewUser();
+				return enSaveResults::svSucceeded;
+			}
+			break;
+		}
+		}
+
+	}
+
+
+
+
+
+
+
+	 static bool IsUserExist(string UserName)
+	{
+		clsUser User1 = clsUser::Find(UserName);
+		return (!User1.IsEmpty());
+	}
+
+	static vector <clsUser> GetUsersList()
+	{
+		return _LoadUsersDataFromFile();
+	}
+	static clsUser GetAddNewUserObject(string UserName)
+	{
+		return clsUser(enMode::enAddNewMode, "", "", "", "", UserName, "", 0);
+	}
+	bool  Delete(string UserName)
+	{
+		vector <clsUser> vUsers;
+		vUsers = _LoadUsersDataFromFile();
+		for (clsUser& User : vUsers)
+		{
+			if (User.UserName == UserName)
+			{
+				User._MarkedForDelete = true;
+				break;
+			}
+		}
+		_SaveUsersDataToFile(vUsers);
+
+	}
+
+
+};
+
